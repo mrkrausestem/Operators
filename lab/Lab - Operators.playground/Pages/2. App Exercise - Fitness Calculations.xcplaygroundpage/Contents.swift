8af8efd51692e2a969012a3a54476631c14e8713
mrkrausestem <33553266+mrkrausestem@users.noreply.github.com>
2024-09-13 11:00:55 -0400

    Updated page 2 instructions

--- lab/Lab - Operators.playground/Pages/2. App Exercise - Fitness Calculations.xcplaygroundpage/Contents.swift
+++ lab/Lab - Operators.playground/Pages/2. App Exercise - Fitness Calculations.xcplaygroundpage/Contents.swift
@@ -3,17 +3,17 @@
  
  >Make sure to include your answer to all questions in a print statement!
  
- Your tutoring app keeps track of users' quiz scores and might also want to display their quiz average. Create three constants, `quizScore1`, `quizScore2`, and `quizScore3`. Give each constant a different value between 60 and 100. Create a constant `quizTotal` equal to the sum of all three test scores. Now create a constant called `quizAverage` that calculates their average score. Print the result. (.50 points)
+ Your tutoring app keeps track of users' quiz scores and might also want to display their quiz average. Create three constants (all integers), `quizScore1`, `quizScore2`, and `quizScore3`. Give each constant a different value between 60 and 100. Create a constant `quizTotal` equal to the sum of all three test scores. Now create a constant called `quizAverage` that calculates their average score. Print the result. (.50 points)
  */
 
 
 /*:
- Now create three more constants, `quizScore1D`, `quizScore2D`, and `quizScore3D`, equal to the same values as `quizScore1`, `quizScore2`, and `quizScore3`. These new constants should be of type `Double`. Create a constant `quizAverageD` equal to the sum of all three of the new constants. Create a constant called `quizAverageD` that that calculates their average score using the Doubles. Print the result. Does this differ from your previous average? Why or why not? (.50 points)
+ Now create three more constants, `quizScore1D`, `quizScore2D`, and `quizScore3D`, equal to the same values as `quizScore1`, `quizScore2`, and `quizScore3`. These new constants should be of type `Double`. Create a constant called `quizAverageD` that that calculates their average score using the Doubles. Print the result. Does this differ from your previous average? Why or why not? (.50 points)
  */
 
 
 /*:
- Imagine that partway through the course a user has passed 15 of the 25 quizzes under the Area and Volumes unit. Create constants `quizzesTaken` and `quizzesGoal`. Both will need to be of type `Double` so that you can perform accurate calculations. `quizzesTaken` should be assigned the value 15, and `quizzesGoal` should be assigned 25. Create a constant `percentOfGoal` that equals an expression that evaluates to the percent of the goal that has been achieved so far. (.50 points)
+ Imagine that part way through the course a user has passed 15 of the 25 quizzes under the Area and Volumes unit. Create constants `quizzesTaken` and `quizzesGoal`. Both will need to be of type `Double` so that you can perform accurate calculations. `quizzesTaken` should be assigned the value 15, and `quizzesGoal` should be assigned 25. Create a constant `percentOfGoal` that equals an expression that evaluates to the percent of the goal that has been achieved so far. (.50 points)
  */
 
 
